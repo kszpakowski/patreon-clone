b52e3170eb7754573a92a91c4f870c3fee200e30
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-01-07 01:23:44 +0100

    add graphql schema for comments

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -37,6 +37,7 @@ export type Mutation = {
   login?: Maybe<LoginResponse>;
   createTier?: Maybe<Tier>;
   createPost: Post;
+  commentPost: CommentPostResponse;
   subscribe?: Maybe<TierSubscription>;
   uploadPostAttachment?: Maybe<UploadResponse>;
   uploadAvatar?: Maybe<UploadResponse>;
@@ -61,6 +62,11 @@ export type MutationCreateTierArgs = {
 
 export type MutationCreatePostArgs = {
   createPostInput: CreatePostInput;
+};
+
+
+export type MutationCommentPostArgs = {
+  commentPostInput?: Maybe<CommentPostInput>;
 };
 
 
@@ -92,6 +98,12 @@ export type UploadResponse = {
 export type LoginResponse = {
   __typename?: 'LoginResponse';
   token?: Maybe<Scalars['String']>;
+  errors?: Maybe<Array<Maybe<Error>>>;
+};
+
+export type CommentPostResponse = {
+  __typename?: 'CommentPostResponse';
+  comment?: Maybe<Comment>;
   errors?: Maybe<Array<Maybe<Error>>>;
 };
 
@@ -151,12 +163,24 @@ export type Post = Node & {
   author: User;
   createdAt: Scalars['Date'];
   attachments?: Maybe<Array<Maybe<Attachment>>>;
+  comments?: Maybe<Array<Maybe<Comment>>>;
 };
 
 export type Attachment = {
   __typename?: 'Attachment';
   url: Scalars['String'];
   name?: Maybe<Scalars['String']>;
+};
+
+export type Comment = Node & {
+  __typename?: 'Comment';
+  id: Scalars['Int'];
+  createdAt: Scalars['Date'];
+  author: Profile;
+  message: Scalars['String'];
+  replies?: Maybe<Array<Maybe<Comment>>>;
+  likes: Scalars['Int'];
+  deleted: Scalars['Boolean'];
 };
 
 export type RegisterUserInput = {
@@ -189,6 +213,11 @@ export type LoginInput = {
 export type PostUploadInput = {
   postId: Scalars['Int'];
   fileName: Scalars['String'];
+};
+
+export type CommentPostInput = {
+  message: Scalars['String'];
+  postId: Scalars['Int'];
 };
 
 
@@ -270,13 +299,14 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
   Date: ResolverTypeWrapper<Scalars['Date']>;
-  Node: ResolversTypes['User'] | ResolversTypes['Tier'] | ResolversTypes['TierSubscription'] | ResolversTypes['Post'];
+  Node: ResolversTypes['User'] | ResolversTypes['Tier'] | ResolversTypes['TierSubscription'] | ResolversTypes['Post'] | ResolversTypes['Comment'];
   Int: ResolverTypeWrapper<Scalars['Int']>;
   Query: ResolverTypeWrapper<{}>;
   String: ResolverTypeWrapper<Scalars['String']>;
   Mutation: ResolverTypeWrapper<{}>;
   UploadResponse: ResolverTypeWrapper<UploadResponse>;
   LoginResponse: ResolverTypeWrapper<LoginResponse>;
+  CommentPostResponse: ResolverTypeWrapper<CommentPostResponse>;
   Error: ResolverTypeWrapper<Error>;
   User: ResolverTypeWrapper<User>;
   Profile: ResolverTypeWrapper<Profile>;
@@ -286,24 +316,27 @@ export type ResolversTypes = {
   Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
   Post: ResolverTypeWrapper<Post>;
   Attachment: ResolverTypeWrapper<Attachment>;
+  Comment: ResolverTypeWrapper<Comment>;
   RegisterUserInput: RegisterUserInput;
   CreateTierInput: CreateTierInput;
   SubscribeTierInput: SubscribeTierInput;
   CreatePostInput: CreatePostInput;
   LoginInput: LoginInput;
   PostUploadInput: PostUploadInput;
+  CommentPostInput: CommentPostInput;
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
   Date: Scalars['Date'];
-  Node: ResolversParentTypes['User'] | ResolversParentTypes['Tier'] | ResolversParentTypes['TierSubscription'] | ResolversParentTypes['Post'];
+  Node: ResolversParentTypes['User'] | ResolversParentTypes['Tier'] | ResolversParentTypes['TierSubscription'] | ResolversParentTypes['Post'] | ResolversParentTypes['Comment'];
   Int: Scalars['Int'];
   Query: {};
   String: Scalars['String'];
   Mutation: {};
   UploadResponse: UploadResponse;
   LoginResponse: LoginResponse;
+  CommentPostResponse: CommentPostResponse;
   Error: Error;
   User: User;
   Profile: Profile;
@@ -313,12 +346,14 @@ export type ResolversParentTypes = {
   Boolean: Scalars['Boolean'];
   Post: Post;
   Attachment: Attachment;
+  Comment: Comment;
   RegisterUserInput: RegisterUserInput;
   CreateTierInput: CreateTierInput;
   SubscribeTierInput: SubscribeTierInput;
   CreatePostInput: CreatePostInput;
   LoginInput: LoginInput;
   PostUploadInput: PostUploadInput;
+  CommentPostInput: CommentPostInput;
 };
 
 export interface DateScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['Date'], any> {
@@ -326,7 +361,7 @@ export interface DateScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes
 }
 
 export type NodeResolvers<ContextType = any, ParentType extends ResolversParentTypes['Node'] = ResolversParentTypes['Node']> = {
-  __resolveType: TypeResolveFn<'User' | 'Tier' | 'TierSubscription' | 'Post', ParentType, ContextType>;
+  __resolveType: TypeResolveFn<'User' | 'Tier' | 'TierSubscription' | 'Post' | 'Comment', ParentType, ContextType>;
   id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
 };
 
@@ -341,6 +376,7 @@ export type MutationResolvers<ContextType = any, ParentType extends ResolversPar
   login?: Resolver<Maybe<ResolversTypes['LoginResponse']>, ParentType, ContextType, RequireFields<MutationLoginArgs, 'loginInput'>>;
   createTier?: Resolver<Maybe<ResolversTypes['Tier']>, ParentType, ContextType, RequireFields<MutationCreateTierArgs, 'createTierInput'>>;
   createPost?: Resolver<ResolversTypes['Post'], ParentType, ContextType, RequireFields<MutationCreatePostArgs, 'createPostInput'>>;
+  commentPost?: Resolver<ResolversTypes['CommentPostResponse'], ParentType, ContextType, RequireFields<MutationCommentPostArgs, never>>;
   subscribe?: Resolver<Maybe<ResolversTypes['TierSubscription']>, ParentType, ContextType, RequireFields<MutationSubscribeArgs, 'subscribeInput'>>;
   uploadPostAttachment?: Resolver<Maybe<ResolversTypes['UploadResponse']>, ParentType, ContextType, RequireFields<MutationUploadPostAttachmentArgs, 'postUploadInput'>>;
   uploadAvatar?: Resolver<Maybe<ResolversTypes['UploadResponse']>, ParentType, ContextType, RequireFields<MutationUploadAvatarArgs, 'fileName'>>;
@@ -355,6 +391,12 @@ export type UploadResponseResolvers<ContextType = any, ParentType extends Resolv
 
 export type LoginResponseResolvers<ContextType = any, ParentType extends ResolversParentTypes['LoginResponse'] = ResolversParentTypes['LoginResponse']> = {
   token?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  errors?: Resolver<Maybe<Array<Maybe<ResolversTypes['Error']>>>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type CommentPostResponseResolvers<ContextType = any, ParentType extends ResolversParentTypes['CommentPostResponse'] = ResolversParentTypes['CommentPostResponse']> = {
+  comment?: Resolver<Maybe<ResolversTypes['Comment']>, ParentType, ContextType>;
   errors?: Resolver<Maybe<Array<Maybe<ResolversTypes['Error']>>>, ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
@@ -414,12 +456,24 @@ export type PostResolvers<ContextType = any, ParentType extends ResolversParentT
   author?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
   createdAt?: Resolver<ResolversTypes['Date'], ParentType, ContextType>;
   attachments?: Resolver<Maybe<Array<Maybe<ResolversTypes['Attachment']>>>, ParentType, ContextType>;
+  comments?: Resolver<Maybe<Array<Maybe<ResolversTypes['Comment']>>>, ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
 export type AttachmentResolvers<ContextType = any, ParentType extends ResolversParentTypes['Attachment'] = ResolversParentTypes['Attachment']> = {
   url?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   name?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type CommentResolvers<ContextType = any, ParentType extends ResolversParentTypes['Comment'] = ResolversParentTypes['Comment']> = {
+  id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  createdAt?: Resolver<ResolversTypes['Date'], ParentType, ContextType>;
+  author?: Resolver<ResolversTypes['Profile'], ParentType, ContextType>;
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  replies?: Resolver<Maybe<Array<Maybe<ResolversTypes['Comment']>>>, ParentType, ContextType>;
+  likes?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  deleted?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
@@ -430,6 +484,7 @@ export type Resolvers<ContextType = any> = {
   Mutation?: MutationResolvers<ContextType>;
   UploadResponse?: UploadResponseResolvers<ContextType>;
   LoginResponse?: LoginResponseResolvers<ContextType>;
+  CommentPostResponse?: CommentPostResponseResolvers<ContextType>;
   Error?: ErrorResolvers<ContextType>;
   User?: UserResolvers<ContextType>;
   Profile?: ProfileResolvers<ContextType>;
@@ -437,6 +492,7 @@ export type Resolvers<ContextType = any> = {
   TierSubscription?: TierSubscriptionResolvers<ContextType>;
   Post?: PostResolvers<ContextType>;
   Attachment?: AttachmentResolvers<ContextType>;
+  Comment?: CommentResolvers<ContextType>;
 };
 
 
