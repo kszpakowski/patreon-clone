c0f65d25072a5848e3620403577379f15ea31cae
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-01-10 18:00:32 +0100

    impememnt posts query resolver

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -17,12 +17,39 @@ export const Query: QueryResolvers = {
       },
     });
   },
-  posts: async () => {
+  posts: async (_, __, { userId }) => {
+    if (!userId) {
+      const posts = await prisma.post.findMany({
+        orderBy: {
+          createdAt: "desc",
+        },
+        include: {
+          tier: {},
+        },
+      });
+      return posts.map((p) => {
+        if (p.tier.level >= 0) {
+          return {
+            id: p.id,
+            createdAt: p.createdAt,
+            title: `This is locked version of post "${p.title}"`,
+            authorId: p.authorId,
+          };
+        } else {
+          return p;
+        }
+      });
+    }
+
     const posts = await prisma.post.findMany({
       orderBy: {
         createdAt: "desc",
       },
+      include: {
+        tier: {},
+      },
     }); //todo implement loading only subscribed and displaying locked posts
+    console.log(posts);
     return posts as any;
   },
 };
